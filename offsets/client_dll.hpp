99e6397a96a0eb4949578f83ec56db5e79f099fa
6y <e404db@outlook.fr>
2026-09-26 10:55:08 +0200

    Update client.dll offsets
    Updated offsets for client.dll to reflect new values.

--- offsets/client_dll.hpp
+++ offsets/client_dll.hpp
@@ -1,17 +1,17 @@
 // wklp cs2 offsets - client.dll
-// generated: 2026-09-15
+// generated: 2026-09-26
 #pragma once
 #include <cstddef>
 #include <cstdint>
 
 namespace cs2_dumper { namespace offsets { namespace client_dll {
-    constexpr std::ptrdiff_t dwEntityList        = 0x27130E8;
-    constexpr std::ptrdiff_t dwLocalPlayerPawn   = 0x255E658;
-    constexpr std::ptrdiff_t dwLocalPlayerController = 0x2535598;
-    constexpr std::ptrdiff_t dwViewMatrix        = 0x25639A0;
-    constexpr std::ptrdiff_t dwViewAngles        = 0x25741C8;
-    constexpr std::ptrdiff_t dwGlowManager       = 0x255A870;
-    constexpr std::ptrdiff_t dwGameRules         = 0x255A858;
-    constexpr std::ptrdiff_t dwPlantedC4         = 0x24C7380;
-    constexpr std::ptrdiff_t dwWeaponC4          = 0x24C2650;
+    constexpr std::ptrdiff_t dwEntityList        = 0x27151A8;
+    constexpr std::ptrdiff_t dwLocalPlayerPawn   = 0x25606D8;
+    constexpr std::ptrdiff_t dwLocalPlayerController = 0x2537628;
+    constexpr std::ptrdiff_t dwViewMatrix        = 0x2565A20;
+    constexpr std::ptrdiff_t dwViewAngles        = 0x2576238;
+    constexpr std::ptrdiff_t dwGlowManager       = 0x255C8F0;
+    constexpr std::ptrdiff_t dwGameRules         = 0x255C8D8;
+    constexpr std::ptrdiff_t dwPlantedC4         = 0x24C9290;
+    constexpr std::ptrdiff_t dwWeaponC4          = 0x24C4550;
 }}}
